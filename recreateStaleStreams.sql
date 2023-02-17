dcb369b9599b708ee4ed846c2f362895df143466
TiMusBhardwaj <bhardwaj.sumit016@gmail.com>
2023-02-17 21:42:56 +0530

    Update recreateStaleStreams.sql

--- recreateStaleStreams.sql
+++ recreateStaleStreams.sql
@@ -2,6 +2,6 @@
 
  show streams in database dbName;
  -- use result scan Id from above query
- select 'create or replace stream '|| "name" || ' on ' || "source_type" || "table_name" ||';' from table(RESULT_SCAN('01aaaacv-7676-b91b-7676-67f507fc247a')) where "name" in 
+ select 'create or replace stream '|| "schema_name"|| '.'|| "name" || ' on ' || "source_type" || ' ' ||"table_name" ||';' from table(RESULT_SCAN('01aaaacv-7676-b91b-7676-67f507fc247a')) where "name" in 
     (<comma separated streams Name > );
  
