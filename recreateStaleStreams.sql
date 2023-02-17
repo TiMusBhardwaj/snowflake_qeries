3ef04d6c0783545e5d9585c61e978b7e49df60dd
TiMusBhardwaj <bhardwaj.sumit016@gmail.com>
2023-02-17 21:40:05 +0530

    Create recreateStaleStreams.sql

---
+++ recreateStaleStreams.sql
@@ -0,0 +1,7 @@
+-- DDL query for stale STREAMS to Recreate them 
+
+ show streams in database dbName;
+ -- use result scan Id from above query
+ select 'create or replace stream '|| "name" || ' on ' || "source_type" || "table_name" ||';' from table(RESULT_SCAN('01aaaacv-7676-b91b-7676-67f507fc247a')) where "name" in 
+    (<comma separated streams Name > );
+ 
